e2ab87a0c0e8666d11766a32a24553928570f1ac
raffe90 <raffe90@gmail.com>
2016-03-11 12:46:49 +0100

    Adds premios

--- resources/views/pages/premios.blade.php
+++ resources/views/pages/premios.blade.php
@@ -9,24 +9,37 @@ El Grupo busca fomentar el desarrollo de prácticas seguras para los usuarios y 
 @stop
 
 @section('content')
-  <ul class="breadcrumb">
-  <li><a href="/"><span class="fa fa-home aqua"></span></a></li>
-  <li><span class="fa fa-trophy"></span> Premios</li>
-</ul>
-
-    <div id="premio" class="row">
+  <section id="premios-container">
+    <header>
+      <ul class="breadcrumb">
+        <li><a href="/"><span class="fa fa-home aqua"></span></a></li>
+        <li><span class="fa fa-trophy"></span> Premios</li>
+      </ul>
+    </header>
+    <article class="row premios">
       <div class="col-md-10 col-md-offset-1">
-        <h2 class="aqua" style="font-size: 1.75em;">I Edición Premio IDCsalud-Quirón a las Mejores Iniciativas en Seguridad del Paciente</h2>
-        <p class="bold">En el marco del Seminario Internacional la consejera de IDCsalud-Quirón, Dra. María Cordón Muro, hará entrega de los citados premios.</p>               
-        <p class="bold">El plazo de presentación de candidaturas está abierto hasta el 10 de mayo de 2015</p>
-        <p>El Grupo IDCsalud-Quirón ha convocado la primera edición del <span class="bold">Premio a las Mejores Iniciativas en Seguridad del Paciente</span>. Con este certamen, el Grupo pretende fomentar el desarrollo de prácticas seguras para los usuarios y profesionales de la sanidad, reconociendo económicamente los mejores proyectos de 2014 y 2015. El plazo de candidaturas a los premios, cuyas bases se pueden consultar a través de <a href="http://www.idcsalud.es" title="idcsalud">idcsalud.es</a> o <a href="http://www.quiron.es" title="quiron salud">quiron.es</a>, está abierto hasta el 10 de mayo.</p>
-        <img src="img/premios.jpg">
-        <p>Con una <span class="bold">dotación económica total de 26.000 euros</span>, esta primera convocatoria cuenta con <span class="bold">4 reconocimientos</span> de entre 15.000 y 2.000 euros para reconocer los proyectos más innovadores en el área de Seguridad del Paciente. El primer y segundo premios, dotados con 15.000 y 7.000 euros respectivamente, serán concedidos a los dos proyectos más relevantes que hayan sido implantados durante el pasado 2014. Además, habrá dos accésit con una aportación económica de 2.000 euros cada uno, que se elegirán entre las iniciativas más novedosas del 2015 para contribuir a la financiación de los proyectos.</p>
-        <p>La candidatura está abierta a todas aquellas personas o entidades con proyectos enmarcados dentro de las 12 áreas de seguridad del paciente establecidas por la OMS. Los aspirantes podrán pertenecer a centros sanitarios tanto públicos como privados, o a instituciones relacionadas con la sanidad como sociedades científicas o asociaciones de pacientes, entre otras, y podrán presentar tantas iniciativas como consideren relevantes para esta primera edición.</p>
-        <p>El <span class="bold">jurado de expertos</span> que seleccionará los proyectos ganadores valorará especialmente la calidad de la metodología del proyecto, el impacto en la organización de la iniciativa y el valor de los resultados obtenidos durante la pre y post intervención de los pacientes, entre otros aspectos. Los trabajos ganadores serán anunciados durante el <span class="bold">2<sup>o</sup> Seminario Internacional de Seguridad del Paciente y Excelencia Clínica</span>, que tendrá lugar el próximo 28 de mayo de 2015 en el Aula Magna de la Fundación Jiménez Díaz en Madrid.</p>
-        <p>Con estos premios, el Grupo IDCsalud-Quirón busca contribuir a la concepción, aplicación y difusión de nuevas iniciativas que refuercen la seguridad del paciente en todos los sistemas sanitarios. Una iniciativa que se enmarca dentro de su Plan Estratégico iniciado en 2012, que marca como línea fundamental la búsqueda de la excelencia asistencial en todos sus centros.
-Las bases se encuentran en el archivo adjunto.</p>
-        <p><span class="fa fa-file-pdf-o red"></span> <a href="http://www.idcsalud.es/es/comunicacion/noticias/grupo-idcsalud-quiron-convoca-i-edicion-premio-mejores-inic.ficheros/126081-I%20Edici%C3%B3n%20Premio%20IDCsalud-Quir%C3%B3n%20DEF.pdf">Bases del concurso (3.5 MB)</a></p>
+        <header>
+          <h1 class="aqua">II Edición Premio Quirónsalud a las Mejores Iniciativas en Seguridad del Paciente.</h1>
+          <h2>Participa y demuestra tu compromiso</h2>        
+        </header>
+        <p>Quirónsalud y la Fundación Quirónsalud han convocado la <span class="bold">II Edición de los Premios a las Mejores Iniciativas en Seguridad del Paciente</span>. con el objetivo de fomentar el desarrollo de prácticas seguras para los usuarios del sistema de salud y sus profesionales. Las candidaturas podrán presentarse hasta el próximo <span class="bold">9 de abril</span> a través de la web <a href="http://www.quironsalud.es" title="quironsalud">quironsalud.es</a>, donde están disponibles las <a href="http://www.quironsalud.es/es/comunicacion/noticias/ii-edicion-premio-quironsalud-mejores-iniciativas-seguridad.ficheros/231205-Bases%20II%20Edici%C3%B3n%20Premio%20Mejores%20%20Iniciativas%20Seguridad%20Paciente.pdf" title="bases">bases</a> con el detalle de la documentación requerida.</p>
+        <img data-hook="premios-poster" src="/img/premios.gif" alt="II Edición Premio Quirónsalud a las Mejores Iniciativas en Seguridad del Paciente.">
+        <p>La convocatoria cuenta con <span class="bold">tres categorías: un primer premio de 15.000 €</span> y un <span class="bold">segundo premio de 7.000 €,</span> para reconocer los dos proyectos más relevantes que hayan sido desarrollados durante 2015; y <span class="bold">un accésit,</span> dotado con <span class="bold">3.000 €,</span> para la mejor iniciativa que tenga previsto su implantación durante este año.</p>
+        <p>Los candidatos a estos premios, personas o entidades, podrán pertenecer a centros sanitarios tanto públicos como privados, o a instituciones relacionadas con la sanidad como sociedades científicas o asociaciones de pacientes, entre otras. Cada autor o grupo de autores podrán presentar una o varias iniciativas.</p>
+        <p>Los trabajos ganadores serán anunciados durante el III Seminario Internacional de Seguridad del Paciente y Excelencia Clínica, organizado por Quirónsalud, que tendrá lugar <span class="bold">el próximo 5 de mayo de 2016 en el Aula Magna de la Fundación Jiménez Díaz en Madrid.</span></p>
+        <p>Estos premios, que responden al compromiso de Quirónsalud, y su Fundación, con la búsqueda de la excelencia asistencial a través de la mejora continua, van en línea con la petición de la Organización Mundial de la Salud (OMS) de prestar la mayor atención posible a la seguridad del paciente y promover políticas y mecanismos basados en la evidencia.</p>
+        <p><span class="fa fa-file-pdf-o red"></span> <a href="http://www.quironsalud.es/en/virtual-press-room/news/ii-edicion-premio-quironsalud-mejores-iniciativas-seguridad.files/231205-Bases%20II%20Edici%C3%B3n%20Premio%20Mejores%20%20Iniciativas%20Seguridad%20Paciente.pdf">Bases II Edición Premio Mejores Iniciativas Seguridad Paciente (1.7 MB) (9 páginas)</a></p>
       </div>      
-    </div>
+    </article>
+  </section>
+@stop
+
+@section('javascript')
+  <script>
+    $(function(){
+      $('img[data-hook="premios-poster"]').on('click', function(e){
+        window.open('http://www.quironsalud.es/en/virtual-press-room/news/ii-edicion-premio-quironsalud-mejores-iniciativas-seguridad', '_blank');
+      })
+    });
+  </script>
 @stop
